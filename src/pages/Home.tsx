d0033c732bac0720a296b2ab483fe310929099c9
Cutiekuan <miks05102002@gmail.com>
2024-03-21 19:58:53 +0800

    added a homepage and simple content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,41 @@
+import React from 'react';
 import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
 } from '@ionic/react';
+import './Home.css'; // Import your CSS file for styling
 
-// CSS
-//import './profile.css';
-
-  const Home: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle>Home</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Home</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <h1>This is my Home page.<br></br><br></br>I am Aileen Obispo, 3rd year IT student of NBSC.</h1>
-        </IonContent>
-      </IonPage>
-    );
-  };
+        <div className="container">
+        <img alt="Silhouette of mountains" id="profile_pic" src="../src/assets/img/PROFPIC_AILEEN_PNG.png" />
+          <div className="content">
+            <h1>Welcome to My Home Page</h1>
+            <p>
+              I am Aileen Obispo, a 3rd-year IT student at NBSC. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Phasellus non
+              convallis libero.
+            </p>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-  //
-  export default Home;
+export default Home;
